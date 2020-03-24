9665962428818694d34b94abc7a923b0a916d856
ahsansaif47 <53190609+ahsansaif47@users.noreply.github.com>
2020-03-24 16:36:38 +0500

    Update Email Extraction(Air India).py

--- Email Extraction(Air India).py
+++ Email Extraction(Air India).py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 import re
 
+## The path for chrome path variable is where chrome driver is installed on my pc.
+## You would provide path where you have installed your chrome driver.
+
 chrome_path = r"C:\Program Files\chromedriver.exe"
 
 driver = webdriver.Chrome(chrome_path)
