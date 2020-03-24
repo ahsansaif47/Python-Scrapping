641edabafd97f17c4c2aeab86899ae2ad777305b
ahsansaif47 <53190609+ahsansaif47@users.noreply.github.com>
2020-03-24 16:35:47 +0500

    Update Wikipedia Title Extract.py

--- Wikipedia Title Extract.py
+++ Wikipedia Title Extract.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 
+## The path for chrome path variable is where chrome driver is installed on my pc.
+## You would provide path where you have installed your chrome driver.
+
 chrome_path = r"C:\Program Files\chromedriver.exe"
 
 driver = webdriver.Chrome(chrome_path)
